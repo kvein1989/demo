a760bbd332ac4240488b9ae1e7bee751b206cc11
jokul2010 <jokul2010@gmail.com>
2013-10-24 08:54:51 +0800

    2013-10-24

--- src/main/java/com/jokul/dao/UserDaoImpl.java
+++ src/main/java/com/jokul/dao/UserDaoImpl.java
@@ -1,13 +1,11 @@
 package com.jokul.dao;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Repository;
